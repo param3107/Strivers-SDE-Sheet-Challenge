6a63460a8b0aafb1cbf4291bc1e51a608d6db090
Param Gupta <101322238+param3107@users.noreply.github.com>
2023-06-06 11:57:29 +0530

    Update 1SetMatrixZeroes.cpp

--- 1SetMatrixZeroes.cpp
+++ 1SetMatrixZeroes.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 void setZeros(vector<vector<int>> &matrix){
     int n = matrix.size();
@@ -29,26 +28,4 @@ void setZeros(vector<vector<int>> &matrix){
         }
     }
     return;
-}
-
-int main(){
-    int T;
-    cin >> T;
-    for(int t = 0; t < T; t++){
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> matrix(n, vector<int> (m));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                cin >> matrix[i][j];
-            }
-        }
-        setZeros(matrix);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                cout << matrix[i][j] << " ";
-            } cout << endl;
-        }
-    }
-    return 0;
 }
