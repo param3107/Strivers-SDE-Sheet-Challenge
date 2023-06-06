a3365dfcc56eae2cc23ce7219fb4c3d6e17aa49d
Param Gupta <101322238+param3107@users.noreply.github.com>
2023-06-06 17:24:13 +0530

    Update 6BestTimeToBuyAndSellStock.cpp

--- 6BestTimeToBuyAndSellStock.cpp
+++ 6BestTimeToBuyAndSellStock.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 
 int maximumProfit(vector<int> &prices){
+    int currentSum = 0, maxSum = 0;
     for(int i = 0; i < prices.size()-1; i++){
         prices[i] = prices[i+1] - prices[i];
-    }
-    int currentSum = 0, maxSum = 0;
-    for(int i = 0; i < prices.size() - 1; i++){
         currentSum += prices[i];
         maxSum = max(maxSum, currentSum);
         if(currentSum < 0) currentSum = 0;
