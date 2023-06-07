d14745399ed8bd3d77ac56e783f8391cea2452fe
Param Gupta <101322238+param3107@users.noreply.github.com>
2023-06-07 17:48:27 +0530

    Update 11MissingAndRepeatingNumbers.cpp

--- 11MissingAndRepeatingNumbers.cpp
+++ 11MissingAndRepeatingNumbers.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 pair<int, int> missingAndRepeating(vector<int> &arr, int n){
-    int index[n] = {0};
-    int miss, repeat;
-    for(int i = 0; i < n; i++){
-        index[arr[i] - 1]++;
-    }
-    for(int i = 0; i < n; i++){
-        if(index[i] == 0) miss = i+1;
-        else if(index[i] == 2) repeat = i+1; 
-    }
-    return make_pair(miss, repeat);
+    long long sum = 0, sqsum = 0;
+	for(int i = 0; i < n; i++){
+		sum += (long long) arr[i];
+		sqsum += ((long long)arr[i])*((long long)arr[i]);
+	}
+	long long diff = (long long) (sum - (long long)n*(n+1)/2);
+	long long sqdiff = (long long) (sqsum - (long long)n*(n+1)*(2*n + 1)/6);
+	return make_pair((int)(sqdiff/diff - diff)/2, (int)(sqdiff/diff + diff)/2);
 }
