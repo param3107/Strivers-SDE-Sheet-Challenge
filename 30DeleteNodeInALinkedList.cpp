c2d483cf18487711c4d0058ac572b779d3ef2861
Param Gupta <101322238+param3107@users.noreply.github.com>
2023-06-15 16:58:04 +0530

    Add files via upload

---
+++ 30DeleteNodeInALinkedList.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+    // template <typename T>
+    // class LinkedListNode
+    // {
+    // public:
+    //     T data;
+    //     LinkedListNode<T> *next;
+    //     LinkedListNode(T data)
+    //     {
+    //         this->data = data;
+    //         this->next = NULL;
+    //     }
+    // };
+
+
+void deleteNode(LinkedListNode<int> * node){
+    LinkedListNode<int>* temp = node;
+    while(temp -> next -> next != NULL){
+        temp -> data = temp -> next -> data;
+        temp = temp -> next;
+    }
+    temp -> data = temp -> next -> data;
+    delete (temp -> next);
+    temp -> next = NULL;
+}
