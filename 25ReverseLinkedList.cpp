819679d8b4e6a7cabe525eb191519d576bc1971a
Param Gupta <101322238+param3107@users.noreply.github.com>
2023-06-15 14:40:46 +0530

    Add files via upload

---
+++ 25ReverseLinkedList.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// template <typename T>
+//     class LinkedListNode
+//     {
+//     public:
+//         T data;
+//         LinkedListNode<T> *next;
+//         LinkedListNode(T data)
+//         {
+//             this->data = data;
+//             this->next = NULL;
+//         }
+//     };
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head){
+    if(head == NULL) return head;
+    LinkedListNode<int>* last = head;
+    LinkedListNode<int>* temp = head -> next;
+    LinkedListNode<int>* mid = head -> next;
+    last -> next = NULL;
+    while(temp != NULL){
+        temp = temp -> next;
+        mid -> next = last;
+        last = mid;
+        mid = temp;
+    }
+    return last;
+}
