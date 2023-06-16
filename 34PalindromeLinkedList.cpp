61fb0c7d22709052cb4b984af8d438eee20e8eae
Param Gupta <101322238+param3107@users.noreply.github.com>
2023-06-16 13:08:18 +0530

    Update 34PalindromeLinkedList.cpp

--- 34PalindromeLinkedList.cpp
+++ 34PalindromeLinkedList.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h> 
 
-    template <typename T>
-    class LinkedListNode
-    {
-    public:
-        T data;
-        LinkedListNode<T> *next;
-        LinkedListNode(T data)
-        {
-            this->data = data;
-            this->next = NULL;
-        }
-    };
+    // template <typename T>
+    // class LinkedListNode
+    // {
+    // public:
+    //     T data;
+    //     LinkedListNode<T> *next;
+    //     LinkedListNode(T data)
+    //     {
+    //         this->data = data;
+    //         this->next = NULL;
+    //     }
+    // };
 
 
 bool isPalindrome(LinkedListNode<int> *head){
