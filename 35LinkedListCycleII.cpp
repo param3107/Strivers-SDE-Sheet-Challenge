bb4aaaa915e9909d9a93e1d049af10e0e1203a3a
Param Gupta <101322238+param3107@users.noreply.github.com>
2023-06-16 13:30:57 +0530

    Add files via upload

---
+++ 35LinkedListCycleII.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+        // class Node
+        // {
+        // public:
+        //     int data;
+        //     Node *next;
+        //     Node()
+        //     {
+        //         this->data = 0;
+        //         next = NULL;
+        //     }
+        //     Node(int data)
+        //     {
+        //         this->data = data;
+        //         this->next = NULL;
+        //     }
+        //     Node(int data, Node* next)
+        //     {
+        //         this->data = data;
+        //         this->next = next;
+        //     }
+        // };
+
+
+Node *firstNode(Node *head){
+    if(head == NULL) return NULL;
+    Node* slow = head, *fast = head;
+    while(fast != NULL && fast -> next != NULL){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+        if(slow == fast){
+            fast = head;
+            while(slow -> next != fast -> next){
+                slow = slow -> next;
+                fast = fast -> next;
+            }
+            return fast -> next;
+        }
+    }
+    return NULL;
+}
